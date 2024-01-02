ce781561ca1e37800ab063ae87fbb0ff4b3c251b
MaxRod30 <max.edw.rod@gmail.com>
2024-01-02 17:53:14 -0300

    Projects

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,46 @@
-/*!
-* Start Bootstrap - Personal v1.0.1 (https://startbootstrap.com/template-overviews/personal)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-personal/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+let formulario = document.getElementById('contactForm');
+let btnEnviar = document.getElementById('submitButton');
+
+
+formulario.onsubmit = (e) => {
+    e.preventDefault();
+    let nombre = e.target[0].value;
+    let mail = e.target[1].value;
+    let number = e.target[2].value;
+    let msg = e.target[3].value;
+
+    let data = {
+        service_id: 'service_9e6cphm',
+        template_id: 'template_m6la34b',
+        user_id: 'hWR3EW16mXhfMAwNI',
+        template_params: {
+            'user_name': nombre,
+            'user_email': mail,
+            'user_celular': number,
+            'message': msg
+        }
+    };
+
+    $.ajax('https://api.emailjs.com/api/v1.0/email/send', {
+        type: 'POST',
+        data: JSON.stringify(data),
+        contentType: 'application/json'
+    }).done(function () {
+        Swal.fire({
+            position: "top-end",
+            icon: "success",
+            title: "Mensaje enviado con éxito",
+            showConfirmButton: false,
+            timer: 1500
+        });
+        formulario.reset();
+    }).fail(function (error) {
+        Swal.fire({
+            icon: "error",
+            title: "Oops... " + JSON.stringify(error),
+            text: "Error al enviar el mensaje",
+          });
+    });
+}
+
+
